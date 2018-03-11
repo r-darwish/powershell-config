240b2c08a7a639a2d559ad10015cd8d15831e9e0
Roey Darwish Dror <roey.ghost@gmail.com>
2018-03-11 20:45:04 +0200

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,39 @@
+$modules = @("PSReadline")
+$windows_modules = @("PSWindowsUpdate")
+
+Set-Variable is_windows -option Constant -value (
+    ($PSVersionTable.PSVersion.Major -lt 6) -Or
+    ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform(
+         [System.Runtime.InteropServices.OSPlatform]::Windows)))
+
+function Install-ModuleIfNeeded ($module)
+{
+    if (-Not (Get-Module -List -Name $module)) {
+        Write-Output "Installing $module"
+        Install-Module -Name $module -Scope CurrentUser -AllowClobber
+    }
+}
+
+function Install-Chocolatey
+{
+    Set-ExecutionPolicy Bypass; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+}
+
+if ($is_windows) {
+    foreach ($module in $windows_modules) {
+        Install-ModuleIfNeeded $module
+    }
+
+	function Add-WUServices {
+		Add-WUServiceManager 7971f918-a847-4430-9279-4a52d1efe18d
+	}
+}
+
+foreach ($module in $modules) {
+    Install-ModuleIfNeeded $module
+}
+
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
