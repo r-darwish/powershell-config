d364c5093d58c910e51eb49fcd653447b668cb9f
Roey Darwish Dror <roey.ghost@gmail.com>
2019-06-24 09:04:10 +0300

    Remove the TokenKind lines
    They seem to only work on some computers. Probably due to version inconsistency

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -80,5 +80,3 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Blue
-Set-PSReadlineOption -TokenKind Command -ForegroundColor Magenta
