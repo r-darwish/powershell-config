b26b7432540fb633d78390966985393755434130
Roey Darwish Dror <roey.ghost@gmail.com>
2020-08-24 14:03:23 +0300

    Add exec workaround in Unix

--- unix.ps1
+++ unix.ps1
@@ -12,3 +12,7 @@ Function Enter-TmuxSession {
 Set-Alias -Name t -Value Enter-TmuxSession
 
 $env:PATH += ":/usr/local/bin:$home/.local/bin"
+
+function exec {
+    Start-Process $args[0] $args[1..$args.Length] -Wait
+}
