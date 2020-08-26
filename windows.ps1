736432eaac60e67a979f468be5104babb520bec2
Roey Darwish Dror <roey.ghost@gmail.com>
2020-08-26 22:01:46 +0300

    Windows fixes

--- windows.ps1
+++ windows.ps1
@@ -3,17 +3,17 @@ function Install-Chocolatey {
 }
 
 function Install-Scoop {
-    Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+    Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')
 }
 
 function Get-Topgrade {
-    $url = (Invoke-WebRequest "https://api.github.com/repos/r-darwish/topgrade/releases/latest" |
-        ConvertFrom-Json |
+    $url = (Invoke-RestMethod "https://api.github.com/repos/r-darwish/topgrade/releases/latest" |
         Select-Object -expand assets |
         Where-Object { $_.name -like '*msvc*' } |
         Select-Object -expand browser_download_url);
 
     Invoke-WebRequest -Uri $url -OutFile topgrade.zip
     Expand-Archive -Path topgrade.zip
-    Remove-Item topgrade.zip
+    Move-Item topgrade/topgrade.exe .
+    Remove-Item topgrade.zip, topgrade
 }
