d6fa19373dfef0b2fbb611cdc3ca690b748182de
Roey Darwish Dror <roey.ghost@gmail.com>
2020-10-05 21:30:45 +0300

    Use Console ConsoleGridView

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ Set-Alias -Name which -Value Get-Command
 
 function Install-NeededModules {
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-    @("PSReadline", "ZLocation", "posh-git").ForEach{ Install-Module $_ -Force }
+    @("PSReadline", "ZLocation", "posh-git", "Microsoft.PowerShell.ConsoleGuiTools").ForEach{ Install-Module $_ -Force }
 
     if (!$IsWindows) {
         Install-Module Microsoft.PowerShell.UnixCompleters
@@ -87,6 +87,50 @@ Set-PSReadLineKeyHandler -Key Ctrl+b -Function CharacterSearchBackward
 Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
 Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
 Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function BackwardKillWord
+function ocgv_history {
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    $selection = $history | Out-ConsoleGridView -Title "Select CommandLine from History" -OutputMode Single -Filter $line
+    if ($selection) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selection)
+        if ($selection.StartsWith($line)) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor)
+        }
+        else {
+            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selection.Length)
+        }    
+    }
+}
+
+$parameters = @{
+    Key              = 'F7'
+    BriefDescription = 'ShowMatchingHistoryOcgv'
+    LongDescription  = 'Show Matching History using Out-ConsoleGridView'
+    ScriptBlock      = {
+        param($key, $arg)   # The arguments are ignored in this example
+
+        $history = Get-History | Sort-Object -Descending -Property Id -Unique | Select-Object CommandLine -ExpandProperty CommandLine 
+        $history | ocgv_history
+    }
+}
+Set-PSReadLineKeyHandler @parameters
+
+$parameters = @{
+    Key              = 'Shift-F7'
+    BriefDescription = 'ShowMatchingGlobalHistoryOcgv'
+    LongDescription  = 'Show Matching History for all PowerShell instances using Out-ConsoleGridView'
+    ScriptBlock      = {
+        param($key, $arg)   # The arguments are ignored in this example
+        $history = [Microsoft.PowerShell.PSConsoleReadLine]::GetHistoryItems().CommandLine 
+        # reverse the items so most recent is on top
+        [array]::Reverse($history) 
+        $history | Select-Object -Unique | ocgv_history
+    }
+}
+Set-PSReadLineKeyHandler @parameters
+
 
 If (-Not (Test-Path Variable:PSise)) {
     Set-Alias ls Get-ChildItemColor -Option AllScope
