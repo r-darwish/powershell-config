5f1fc8c95446c1ddcdc312177b259bd99d999b68
Roey Darwish Dror <roey.ghost@gmail.com>
2021-03-30 10:33:59 +0300

    Abandon Scoop

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,26 +3,6 @@ if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     choco feature enable -n allowGlobalConfirmation
 }
-
-if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing scoop"
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-}
-
-function InstallScoopDependency {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position = 0)]
-        [string[]]
-        $Dependency
-    )
-
-    foreach ($dependency in $Dependency) {
-        Write-Host "Installing $dependency via Scoop"
-        scoop install $dependency
-    }
-}
-
 function InstallChocoDependency {
     [CmdletBinding()]
     param (
@@ -37,7 +17,6 @@ function InstallChocoDependency {
     }
 }
 
-InstallScoopDependency @("fzf", "git", "starship", "fd")
-InstallChocoDependency @("powershell-core", "microsoft-windows-terminal")
+InstallChocoDependency @("powershell-core", "microsoft-windows-terminal", "starship", "git")
 git clone https://github.com/r-darwish/powershell-config (& 'C:\Program Files\PowerShell\7\pwsh.exe' -c 'Split-Path -parent $profile')
 &'C:\Program Files\PowerShell\7\pwsh.exe' -c "Install-NeededModules"
