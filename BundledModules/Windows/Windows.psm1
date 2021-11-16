4f2a7fd24b96db93897dab816fd25752a3caa196
Roey Darwish Dror <roey.ghost@gmail.com>
2021-11-16 21:45:22 +0200

    Add commands for Windows Sandbox

--- BundledModules/Windows/Windows.psm1
+++ BundledModules/Windows/Windows.psm1
@@ -18,6 +18,76 @@ function Install-Topgrade {
     Remove-Item topgrade.zip, topgrade
 }
 
+function New-WindowsSandbox {
+    [CmdletBinding()]
+    param (
+        # Name of the box
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Name,
+
+        # vGPU
+        [Parameter()]
+        [bool]
+        $VGPU = $true,
+
+        # Mapped Folders
+        [Parameter()]
+        [string[]]
+        $Folders,
+
+        # Launch
+        [Parameter()]
+        [switch]
+        $Launch
+    )
+
+    # Set The Formatting
+    $xmlsettings = New-Object System.Xml.XmlWriterSettings
+    $xmlsettings.Indent = $true
+    $xmlsettings.IndentChars = "    "
+    $xmlsettings.OmitXmlDeclaration = $true
+    $xmlsettings.ConformanceLevel = "Fragment"
+    $filename = "$Name.wsb"
+
+    # Set the File Name Create The Document
+    $XmlWriter = [System.XML.XmlWriter]::Create($filename, $xmlsettings)
+
+
+
+    # Start the Root Element
+    $xmlWriter.WriteStartElement("Configuration")
+
+    if ($VGPU) {
+        $XmlWriter.WriteStartElement("vGPU")
+        $XmlWriter.WriteString("Enable")
+        $XmlWriter.WriteEndElement()
+    }
+
+    $XmlWriter.WriteStartElement("MappedFolders")
+    for ($i = 0; $i -lt $Folders.Count; $i++) {
+        $XmlWriter.WriteStartElement("MappedFolder")
+        $split = $Folders[$i] -split '/'
+        
+        $absPath = Resolve-Path $split[0]
+        $XmlWriter.WriteElementString("HostFolder", $absPath)
+
+        if ($split[1]) {
+            $XmlWriter.WriteElementString("SandboxFolder", $split[1])
+        }
+
+        $XmlWriter.WriteEndElement()
+    }
+    $XmlWriter.WriteEndElement()
+
+    $xmlWriter.Flush()
+    $xmlWriter.Close()
+
+    if ($Launch) {
+        & ./$filename
+    }
+}
+
 function Test-Admin {
     ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
